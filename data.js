e88811e11a8b61e1a46427efbb9546093ef66edd
krquinton <32116802+krquinton@users.noreply.github.com>
2021-04-13 16:15:35 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,61 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0263",
+      "name": "DJI_0263",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.0535503295635777,
+          "pitch": 0.33857142937349494,
+          "title": "Bahay Ni Quinton",
+          "text": "Bahay namin"
+        },
+        {
+          "yaw": 1.7194188712001885,
+          "pitch": 0.17563432066464202,
+          "title": "Grocery ni Mama",
+          "text": "Mura dito ang bigas. Char"
+        },
+        {
+          "yaw": -2.505248776878968,
+          "pitch": 1.1560948042038834,
+          "title": "Tindahan ni Kamahalan",
+          "text": "juskopurudy, ang mahal ng tinda"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
