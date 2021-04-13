d94264f66cac3e12410839de8d1f45534e90e690
krquinton <32116802+krquinton@users.noreply.github.com>
2021-04-13 17:08:57 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -28,140 +28,34 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -2.1524824308229196,
-          "pitch": 0.3918965756110495,
-          "rotation": 0,
-          "target": "1-dji_0237"
-        },
-        {
-          "yaw": -1.822958233279742,
-          "pitch": 0.1963910871103458,
-          "rotation": 0,
-          "target": "2-dji_0238"
-        },
-        {
-          "yaw": -3.0584208940408164,
-          "pitch": 0.32457702239967645,
-          "rotation": 0,
-          "target": "3-dji_0239"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": [
+        {
+          "yaw": -2.0535503295635777,
+          "pitch": 0.33857142937349494,
+          "title": "Bahay Ni Quinton",
+          "text": "Bahay namin"
+        },
         {
           "yaw": 1.7194188712001885,
           "pitch": 0.17563432066464202,
-          "title": "NuCiti Central",
-          "text": "Grocery"
+          "title": "Grocery ni Mama",
+          "text": "Mura dito ang bigas. Char"
         },
         {
           "yaw": -2.505248776878968,
           "pitch": 1.1560948042038834,
-          "title": "Aling Yang Store",
-          "text": "Block5 SMV"
+          "title": "Tindahan ni Kamahalan",
+          "text": "juskopurudy, ang mahal ng tinda"
         }
       ]
-    },
-    {
-      "id": "1-dji_0237",
-      "name": "DJI_0237",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-dji_0238",
-      "name": "DJI_0238",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-dji_0239",
-      "name": "DJI_0239",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
